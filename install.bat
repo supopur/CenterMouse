5289828772254fc9011eb95720ab9374953b5789
Projectmat1 <matylda.smyk.08@gmail.com>
2021-09-20 21:33:31 +0200

    added start the script option

--- install.bat
+++ install.bat
@@ -1,6 +1,14 @@
-echo 'don't worry about errors'
+@echo off
+echo don't worry about errors
 pip install pyautogui
 pip install keyboard
 pip install time
-echo 'if you saw any errors don't panic'
-PAUSE
+echo if you saw any errors don't panic
+echo Do you want to launch the script? (y/n)
+set /p Input=Enter Yes or No:
+If /I "%Input%"=="y" goto yes
+goto no
+:yes
+python main.py
+:no
+pause
