4239894482a416f45334c3141503b671e20d63c5
Projectmat1 <matylda.smyk.08@gmail.com>
2021-09-20 21:10:43 +0200

    added better error handling

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-import pyautogui
-import keyboard
-import time
+W  = '\033[0m'  # white (normal)
+R  = '\033[31m' # red
+G  = '\033[32m' # green
 
-x, y= pyautogui.size()
-x /= 2 
-y /= 2
-while True:
-    if keyboard.is_pressed('q'):
-        try:
-            pyautogui.moveTo(x, y, duration = 0)
-        except:
-            print('error')
-    else:
-        time.sleep(.1)
+
+def center():
+    #gets display ressolution
+    x, y= pyautogui.size()
+    x /= 2 
+    y /= 2
+    
+    while True:
+        if keyboard.is_pressed('q'):
+            try:
+                #moves the mouse to the center of the screen
+                #the duration value means how fast will the mouse get to the center
+                pyautogui.moveTo(x, y, duration = 0)
+            except:
+                print('error')
+
+        else: #if you wanna fix the script sometimes not registering key presses remove this line
+            time.sleep(.1) #and this line too
+            
+try:
+    import pyautogui
+    import keyboard
+    import time
+except:
+    print(R+'ERROR IMPORTING MODULES'+W)
+else:
+    print(G+'No exception occured'+W)
+    center()
