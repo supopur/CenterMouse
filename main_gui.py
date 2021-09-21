e4d12aabd661061987c226fc6ee0f2e2f23d04b6
Projectmat1 <matylda.smyk.08@gmail.com>
2021-09-21 17:30:20 +0200

    the gui finally works

--- main_gui.py
+++ main_gui.py
@@ -8,16 +8,8 @@ def center():
     x /= 2 
     y /= 2
 
-    ws = Tk()
-    ws.title('Mouse center')
-    def stop():
-        ws.destroy()
-        sys.exit(W+'Successfully stopped'+W)
-    Button(ws, text="Stop", command=stop).pack(pady=20)
-    ws.mainloop()
-
-    
-    while True:
+    def more_center():
+        print('the more_center function was called')
         if keyboard.is_pressed('q'):
             try:
                 #moves the mouse to the center of the screen
@@ -25,19 +17,31 @@ def center():
                 pyautogui.moveTo(x, y, duration = 0)
             except:
                 print('error')
-
-        else: #if you wanna fix the script sometimes not registering key presses remove this line
-            time.sleep(.1) #and this line too
+        time.sleep(.1)
+        ws.after(0, more_center)
+        
+    
+    ws = Tk()
+    ws.title('Mouse center')
+    def stop():
+        ws.destroy()
+        sys.exit(W+'Successfully stopped'+W)
+    Button(ws, text="Stop", command=stop).pack(pady=20)
+    ws.update()
+    ws.after(0, more_center)
+    ws.mainloop()
     
 
             
             
 try:
-    import pyautogui
+    
     import keyboard
     import time
     from tkinter import *
     import sys
+    import pyautogui
+    
 except:
     print(R+'ERROR IMPORTING MODULES'+W)
 else:
